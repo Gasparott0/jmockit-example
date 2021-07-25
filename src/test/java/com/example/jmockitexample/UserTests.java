bba341cd71efce36872292935fa544c467ff4ed8
Gasparotto <mateusg@cpqd.com.br>
2021-07-25 14:32:31 -0300

    correcao

--- src/test/java/com/example/jmockitexample/UserTests.java
+++ src/test/java/com/example/jmockitexample/UserTests.java
@@ -2,12 +2,14 @@ package com.example.jmockitexample;
 
 import com.example.jmockitexample.domain.model.AddressViacep;
 import com.example.jmockitexample.domain.model.User;
+import com.example.jmockitexample.domain.repository.UserRepository;
 import com.example.jmockitexample.domain.service.UserService;
 import com.example.jmockitexample.domain.service.ViacepService;
 import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Tested;
 import mockit.Verifications;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,15 +22,20 @@ public class UserTests {
     private UserService userService;
 
     @Test
-    public void testViacepService(@Injectable ViacepService mockViacepService) {
+    public void testViacepService(@Injectable ViacepService mockViacepService, @Injectable UserRepository mockUserRepository) {
+
+        var user = new User();
+
         new Expectations() {{
 
             mockViacepService.loadUserAddressByCep("13186587");
             result = loadAddressViacep();
 
+            mockUserRepository.save(user);
+            result = loadUser();
+
         }};
 
-        var user = new User();
         user.setCep("13186587");
         var userResponse = userService.saveUser(user);
 
@@ -40,6 +47,7 @@ public class UserTests {
     }
 
     private AddressViacep loadAddressViacep() {
+
         var addressViacep = new AddressViacep();
         addressViacep.setCep("13186-587");
         addressViacep.setLogradouro("Rua Andiroba");
@@ -53,6 +61,22 @@ public class UserTests {
         addressViacep.setSiafi("2951");
 
         return addressViacep;
+    }
+
+    private User loadUser() {
+
+        var user = new User();
+        user.setId(1L);
+        user.setName("Mateus");
+        user.setLastName("Gasparotto");
+        user.setHouseNumber("128");
+        user.setComplement("A");
+        user.setCep("13186587");
+        user.setStreet("Rua Andiroba");
+        user.setDistrict("Jardim Nova Alvorada");
+        user.setLocation("Hortolândia");
+
+        return user;
     }
 
 }
